c5ae6f08f34d5563efb394236775556538ef38ef
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-30 17:20:49 +0200

    Added MyCategoriesAdapter.kt, onBindViewHolder, onCreateViewHolder, getItemCount

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/Common/Common.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/Common/Common.kt
@@ -3,6 +3,8 @@ package com.ganarstudio.orderfoodappkotlin.Common
 import com.ganarstudio.orderfoodappkotlin.Model.UserModel
 
 object Common {
+    val FULL_WIDTH_COLUMN: Int = 1
+    val DEFAULT_COLUMN_COUNT: Int = 0
     val BEST_DEALS_REF: String = "BestDeals"
     val POPULAR_REF: String = "MostPopular"
     val USER_REFERENCE = "Users"
