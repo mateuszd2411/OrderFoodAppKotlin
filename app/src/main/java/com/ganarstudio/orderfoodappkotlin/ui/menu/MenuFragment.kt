bbdf38fce0faab23cacfd8d12eef4070cbf3f322
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-02 13:48:11 +0200

    Started initViews() in MenuFragment.kt, added library for SpotsDialog

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/menu/MenuFragment.kt
@@ -4,28 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ganarstudio.orderfoodappkotlin.R
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.fragment_category.*
 
 class MenuFragment : Fragment() {
 
-    private lateinit var galleryViewModel: MenuViewModel
+    private lateinit var menuViewModel: MenuViewModel
+    private lateinit var dialog: android.app.AlertDialog
+    private lateinit var layoutAnimationController:LayoutAnimationController
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
+        menuViewModel =
             ViewModelProviders.of(this).get(MenuViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_category, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
+
+        initViews()
+
         galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
+    }
+
+    private fun initViews() {
+        dialog = SpotsDialog.Builder().setContext(context)
+            .setCancelable(false).build()
+        dialog.show()
+        layoutAnimationController = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_item_from_left)
+        recycler_menu!!.setHasFixedSize(true)
+
     }
 }
