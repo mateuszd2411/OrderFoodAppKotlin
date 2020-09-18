df945eb1e7f576d6de7777dc5500f80803f55e40
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-18 12:35:09 +0200

    Set FoodListViewModel.kt

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/foodlist/FoodListViewModel.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/foodlist/FoodListViewModel.kt
@@ -1,4 +1,18 @@
 package com.ganarstudio.orderfoodappkotlin.ui.foodlist
 
-class FoodListViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ganarstudio.orderfoodappkotlin.Common.Common
+import com.ganarstudio.orderfoodappkotlin.Model.FoodModel
+
+class FoodListViewModel : ViewModel() {
+
+    private var mutableFoodModelListData: MutableLiveData<List<FoodModel>>? = null
+
+    fun getMutableFoodModelListData(): MutableLiveData<List<FoodModel>> {
+        if (mutableFoodModelListData == null)
+            mutableFoodModelListData = MutableLiveData()
+        mutableFoodModelListData!!.value = Common.categorySelected!!.foods
+        return mutableFoodModelListData!!
+    }
 }
