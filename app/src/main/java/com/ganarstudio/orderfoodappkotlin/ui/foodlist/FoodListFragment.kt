fae31b7e7be2e92919ab4042979e7923a2a7f5d2
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-19 12:53:57 +0200

    Added recycler view to fragment_food_list.xml, set fragment_food_list.xml in FoodListFragment.kt, set animation when init

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/foodlist/FoodListFragment.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/foodlist/FoodListFragment.kt
@@ -4,19 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ganarstudio.orderfoodappkotlin.Adapter.MyFoodListAdapter
 import com.ganarstudio.orderfoodappkotlin.R
 
 class FoodListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private lateinit var foodListViewModel: FoodListViewModel
+
+    var recycler_food_list: RecyclerView? = null
+    var layoutAnimationController: LayoutAnimationController? = null
+
+    var adapter: MyFoodListAdapter? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_list, container, false)
+        foodListViewModel =
+            ViewModelProviders.of(this).get(FoodListViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_food_list, container, false)
+        initViews(root)
+        foodListViewModel.getMutableFoodModelListData().observe(this, Observer {
+            adapter = MyFoodListAdapter(context!!, it)
+            recycler_food_list!!.adapter = adapter
+            recycler_food_list!!.layoutAnimation = layoutAnimationController
+        })
+        return root
     }
+
+    private fun initViews(root: View?) {
+        recycler_food_list = root!!.findViewById(R.id.recycler_food_list) as RecyclerView
+        recycler_food_list!!.setHasFixedSize(true)
+        recycler_food_list!!.layoutManager = LinearLayoutManager(context)
+
+        layoutAnimationController = AnimationUtils.loadLayoutAnimation(context,R.anim.layout_item_from_left)
+    }
+
 }
