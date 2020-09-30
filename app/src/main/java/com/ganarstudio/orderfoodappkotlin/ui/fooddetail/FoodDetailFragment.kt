cefe203ae3209ef38fcdb7111e4b3d68b8880afb
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-30 11:08:18 +0200

    initView in FoodDetailFragment.kt

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailFragment.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailFragment.kt
@@ -4,28 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.andremion.counterfab.CounterFab
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton
+import com.ganarstudio.orderfoodappkotlin.Model.FoodModel
 import com.ganarstudio.orderfoodappkotlin.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_food_detail.*
 
 class FoodDetailFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: FoodDetailViewModel
+    private lateinit var foodDetailViewModel: FoodDetailViewModel
+
+    private var img_food: ImageView? = null
+    private var btnCart: CounterFab? = null
+    private var btnRating: FloatingActionButton? = null
+    private var food_name: TextView? = null
+    private var food_description: TextView? = null
+    private var food_price: TextView? = null
+    private var number_button: ElegantNumberButton? = null
+    private var ratingBar: RatingBar? = null
+    private var btnShowComment: Button? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
+        foodDetailViewModel =
             ViewModelProviders.of(this).get(FoodDetailViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_food_detail, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        initView(root)
+        foodDetailViewModel.getMutableLiveDataFood().observe(viewLifecycleOwner, Observer {
+            displayInfo(it)
         })
         return root
+    }
+
+    private fun displayInfo(it: FoodModel?) {
+
+    }
+
+    private fun initView(root: View?) {
+        btnCart = root!!.findViewById(R.id.btnCart) as CounterFab
+        img_food = root!!.findViewById(R.id.img_food) as ImageView
+        btnRating = root!!.findViewById(R.id.btn_rating) as FloatingActionButton
+        food_name = root!!.findViewById(R.id.food_name) as TextView
+        food_price = root!!.findViewById(R.id.txt_food_price) as TextView
+        food_description = root!!.findViewById(R.id.food_description) as TextView
+        number_button = root!!.findViewById(R.id.number_button) as ElegantNumberButton
+        ratingBar = root!!.findViewById(R.id.ratingBar) as RatingBar
+        btnShowComment = root!!.findViewById(R.id.btnShowComment) as Button
     }
 }
