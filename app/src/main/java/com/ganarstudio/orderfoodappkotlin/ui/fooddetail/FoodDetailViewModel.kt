8ff0ac6efb36156e6bbfc8c58d1d53359b2dbe33
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-30 10:57:28 +0200

    Set FoodDetailViewModel.kt mutableLiveData

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailViewModel.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailViewModel.kt
@@ -3,11 +3,17 @@ package com.ganarstudio.orderfoodappkotlin.ui.fooddetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ganarstudio.orderfoodappkotlin.Common.Common
+import com.ganarstudio.orderfoodappkotlin.Model.FoodModel
 
 class FoodDetailViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+    private var mutableLiveData: MutableLiveData<FoodModel> ?= null
+
+    fun getMutableLiveDataFood(): MutableLiveData<FoodModel> {
+        if (mutableLiveData == null)
+            mutableLiveData = MutableLiveData()
+        mutableLiveData!!.value = Common.foodSelected
+        return mutableLiveData!!
     }
-    val text: LiveData<String> = _text
 }
