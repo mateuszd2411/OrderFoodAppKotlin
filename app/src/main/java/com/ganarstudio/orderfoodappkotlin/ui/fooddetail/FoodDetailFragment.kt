000b4b8b305bf012ab488ce7dc239e72baca726e
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-30 11:15:18 +0200

    set displayInfo in FoodDetailFragment.kt(error with price)

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailFragment.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/fooddetail/FoodDetailFragment.kt
@@ -12,11 +12,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.andremion.counterfab.CounterFab
+import com.bumptech.glide.Glide
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton
 import com.ganarstudio.orderfoodappkotlin.Model.FoodModel
 import com.ganarstudio.orderfoodappkotlin.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_food_detail.*
+import java.lang.StringBuilder
 
 class FoodDetailFragment : Fragment() {
 
@@ -27,7 +29,7 @@ class FoodDetailFragment : Fragment() {
     private var btnRating: FloatingActionButton? = null
     private var food_name: TextView? = null
     private var food_description: TextView? = null
-    private var food_price: TextView? = null
+    //private var food_price: TextView? = null
     private var number_button: ElegantNumberButton? = null
     private var ratingBar: RatingBar? = null
     private var btnShowComment: Button? = null
@@ -49,7 +51,10 @@ class FoodDetailFragment : Fragment() {
     }
 
     private fun displayInfo(it: FoodModel?) {
-
+        Glide.with(context!!).load(it!!.image).into(img_food!!)
+        food_name!!.text = StringBuilder(it!!.name!!)
+        food_description!!.text = StringBuilder(it!!.description!!)
+        //food_price!!.text = StringBuilder(it!!.price!!.toString())
     }
 
     private fun initView(root: View?) {
@@ -57,7 +62,7 @@ class FoodDetailFragment : Fragment() {
         img_food = root!!.findViewById(R.id.img_food) as ImageView
         btnRating = root!!.findViewById(R.id.btn_rating) as FloatingActionButton
         food_name = root!!.findViewById(R.id.food_name) as TextView
-        food_price = root!!.findViewById(R.id.txt_food_price) as TextView
+//        food_price = root!!.findViewById(R.id.txt_food_price) as TextView
         food_description = root!!.findViewById(R.id.food_description) as TextView
         number_button = root!!.findViewById(R.id.number_button) as ElegantNumberButton
         ratingBar = root!!.findViewById(R.id.ratingBar) as RatingBar
