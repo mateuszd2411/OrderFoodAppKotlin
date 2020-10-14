172af99bdb9103cc6de9fd9027695af6e13fd2a3
mateuszd2411 <mateuszd2411@gmail.com>
2020-10-14 12:35:45 +0200

    init, set recycler view in CommentFragment.kt

--- app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/comment/CommentFragment.kt
+++ app/src/main/java/com/ganarstudio/orderfoodappkotlin/ui/comment/CommentFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ganarstudio.orderfoodappkotlin.Callback.ICommentCallBack
 import com.ganarstudio.orderfoodappkotlin.Model.CommentModel
 import com.ganarstudio.orderfoodappkotlin.R
@@ -17,11 +20,12 @@ class CommentFragment : BottomSheetDialogFragment(), ICommentCallBack {
 
     private  var commentViewModel: CommentViewModel ?= null
 
+    private var recycyler_comment:RecyclerView?=null
+
     private var listener: ICommentCallBack
 
     init {
         listener = this
-        commentViewModel = ViewModelProviders.of(this).get(CommentViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -30,7 +34,19 @@ class CommentFragment : BottomSheetDialogFragment(), ICommentCallBack {
         savedInstanceState: Bundle?
     ): View? {
         val itemView = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_comment_fragment, container, false)
-        return super.onCreateView(inflater, container, savedInstanceState)
+        initViews(itemView)
+        return itemView
+    }
+
+    private fun initViews(itemView: View?) {
+        commentViewModel = ViewModelProviders.of(this).get(CommentViewModel::class.java)
+
+
+        recycyler_comment = itemView!!.findViewById(R.id.recycler_comment) as RecyclerView
+        recycyler_comment!!.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, true)
+        recycyler_comment!!.layoutManager = layoutManager
+        recycyler_comment!!.addItemDecoration(DividerItemDecoration(requireContext(), layoutManager.orientation))
     }
 
 
